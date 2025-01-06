de24ff427d0c15f532a1295f453f350f90fe1a26
Nataliya Kim <magistr1982@gmail.com>
2025-01-06 11:27:12 +0200

    Change metro and tailwind config files

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 
 const Index = () => {
   return (
     <View>
+      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
