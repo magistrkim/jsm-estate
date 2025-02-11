cf01e0df752685adedf1fbded57f3e59a81ad1d5
Nataliya Kim <magistr1982@gmail.com>
2025-02-11 17:01:37 +0200

    Create SignIn Page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,30 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
 
 const SignIn = () => {
   return (
     <SafeAreaView className="bg-white h-full">
-      <ScrollView>
-        <Text>SignIn</Text>
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base font-rubik text-black-200 uppercase text-center">
+            Welcome to ReState
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 mt-2 text-center">
+            Let`s Get You Closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to ReState with Google
+          </Text>
+          <TouchableOpacity></TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
