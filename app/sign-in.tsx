97cccd6abd4b899e42b212353cde035aa1bb74ac
Nataliya Kim <magistr1982@gmail.com>
2025-02-12 10:20:36 +0200

    Complete SignIn UI

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -2,8 +2,12 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {
+    console.log("Press the Button");
+  };
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
@@ -23,7 +27,21 @@ const SignIn = () => {
           <Text className="text-lg font-rubik text-black-200 text-center mt-12">
             Login to ReState with Google
           </Text>
-          <TouchableOpacity></TouchableOpacity>
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-400 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
