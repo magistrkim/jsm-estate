347917e266755072ee1c616f931955991cd2ace8
Nataliya Kim <magistr1982@gmail.com>
2025-02-12 11:05:54 +0200

    Create new appwrite Client

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,14 @@
+import { Client } from "react-native-appwrite";
+
 export const config = {
   platform: "com.jsm.restate",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
 };
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
