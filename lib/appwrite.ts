88f65cf68a13007ac92430c8b95cf4e8dfcb2e54
Nataliya Kim <magistr1982@gmail.com>
2025-03-03 14:48:42 +0200

    Create login function in appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,6 @@
-import { Account, Avatars, Client } from "react-native-appwrite";
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import * as Linking from "expo-linking";
+import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
   platform: "com.jsm.restate",
@@ -15,3 +17,29 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+
+export const login = async () => {
+  try {
+    const redirectUri = Linking.createURL("/");
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+    if (!response) throw new Error("Failed to login");
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
+    if (browserResult.type !== "success") throw new Error("Failed to login");
+    const url = new URL(browserResult.url);
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+    if (!secret || !userId) throw new Error("Failed to login");
+    const session = await account.createSession(userId, secret);
+    if (!session) throw new Error("Failed to create a session");
+    return session;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
